2af3d235207821ba1b7c25d87e3b16d6c4b5679f
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-11-11 12:55:59 -0300

    Add solution file

--- my_list.rb
+++ my_list.rb
@@ -12,20 +12,3 @@ class MyList
     (0...@list.length).each { |item| yield @list[item] }
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-
-p list.list
-puts('Test #all?')
-puts(list.all? { |e| e < 5 })
-# true
-puts(list.all? { |e| e > 5 })
-# false
-puts('Test #any?')
-puts(list.any? { |e| e == 2 })
-# true
-puts(list.any? { |e| e == 5 })
-# false
-puts('Test #filter')
-p(list.filter(&:even?))
-# [2. 4]
